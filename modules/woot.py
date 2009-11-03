65d5c81510346c866c8bdfaa4179a2c82bd65c99
Cory Perry <killogge@gmail.com>
2009-11-03 18:35:57 -0500

    Removed buyout link for woot module
    * Due to popular demand, woot module now only provides link to woot site, not the buyout page.

--- modules/woot.py
+++ modules/woot.py
@@ -17,32 +17,12 @@ def PROCESS(bot, args, text):
 	if len(product)>50: product=product[:47]+'...'
 	percent=e.findtext('/channel/item/{http://www.woot.com/}soldout')
 	wootoff=e.findtext('/channel/item/{http://www.woot.com/}wootoff')
-	wootoff = 'I want one!' if wootoff=='False' else 'WootOff!'
-	url=e.findtext('/channel/item/{http://www.woot.com/}purchaseurl')
+	wootoff='' if wootoff=='False' else 'WootOff! '
 	if percent == 'False':
 		percent=100*float(e.findtext('channel/item/{http://www.woot.com/}soldoutpercentage'))
 		percent="%.2f%%" % (percent)
-		if TINYURL: url=getTiny(url)
-	else:
-		url="http://%s.com" % woot
-	bot.mesg(u"%s \"%s\" %s: %s %s" % (price, product, percent, wootoff, url), args[1])
+	else: percent='SOLD OUT'
+	url="http://%s.com/" % woot
+	bot.mesg(u"%s \"%s\" %s sold: %s%s" % (price, product, percent, wootoff, url), args[1])
 	f.close()
 	return False
-def INIT(bot):
-	global DOTINYURL, TINYURL
-	if not bot.config.has_section('woot'): return
-	DOTINYURL = bot.config.getboolean('woot','usetinyurl')
-	TINYURL = bot.config.get('woot','tinyurl') if DOTINYURL else None
-def getTiny(url):
-	"""Returns the TinyURL version of <url>"""
-	TINYURL,tinyurl='http://tinyurl.com/api-create.php?','N/A'
-	try:
-		param=urllib.urlencode({'url':url})
-		f=urllib.urlopen("%s%s" % (TINYURL, param))
-		ret=f.read().rstrip()
-		if re.match(r'(?i)^(http:\/\/\S+)$',ret): tinyurl=ret
-		else: raise Exception("Bad URL returned: %s" % (ret))
-	except Exception as e: raise e
-	finally:
-		if f: f.close()
-	return tinyurl
