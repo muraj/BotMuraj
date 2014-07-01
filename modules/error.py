74a75ed689ac973c5075a38fd0ad3518066b00d7
muraj <killogge@gmail.com>
2014-06-30 21:01:17 -0600

    Removing old bot files

--- modules/error.py
+++
@@ -1,9 +0,0 @@
-"""This module raises an error within the bot. This modules is purely for testing/debugging purposes of the internals of the bot only."""
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-RULE=r'^error$'
-PRIORITY=0
-COMMAND='PRIVMSG'
-DIRECTED=1	#Must be directed to the bot
-def PROCESS(bot, args, text):
-	raise Exception('OMG! ERROR!!')
