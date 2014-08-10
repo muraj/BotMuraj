6f8826a2cd1eab7497113fd3f6735912fbb61904
muraj <killogge@gmail.com>
2014-08-10 15:16:37 -0600

    Fixing epenis module

--- plugins/epenis/epenis.py
+++ plugins/epenis/epenis.py
@@ -6,8 +6,8 @@ SCALE = 80.0 / 31556926.0
 
 def uptime(bot, user, channel, args, penis=False):
   t = time.time() - START_TIME
-  minutes = t / 60
-  seconds = t - minutes
+  minutes = int(t / 60)
+  seconds = t - minutes * 60
   bot.say(channel, "I've been up for %d minutes and %d seconds" % (minutes, seconds))
   if penis:
     shaft_len = int(t * SCALE)
