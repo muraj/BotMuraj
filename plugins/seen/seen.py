a5b8fdd9566d1019bf15633bed9bfc60c85e96d1
muraj <killogge@gmail.com>
2014-08-17 15:53:48 -0600

    Removing some debug from seen

--- plugins/seen/seen.py
+++ plugins/seen/seen.py
@@ -8,7 +8,7 @@ def seen_track(bot, user, channel, msg):
   global seen_db
   user, _, _ = user.partition('!')
   seen_db[user] = time.time()
-  bot.log.msg("Saw %s at %s" % (user, time.ctime(seen_db[user])))
+  #bot.log.msg("Saw %s at %s" % (user, time.ctime(seen_db[user])))
   return True # Continue processing this message
 
 @command('seen')
