a082ffa36787d80c1eef00f997458d7cf5861ee9
muraj <killogge@gmail.com>
2014-10-22 20:56:13 -0500

    Changed active_mon to keep track of scores on a channel by channel basis

--- plugins/active_mon/active_mon.py
+++ plugins/active_mon/active_mon.py
@@ -10,7 +10,8 @@ cmd = 'souls'
 def activity_track(bot, user, channel, msg):
   global active_db, reducefn
   user, _, _ = user.partition('!')
-  active_db[user.lower()] = active_db.get(user.lower(), 0) + reducefn(msg)
+  user_chan = user.lower() + '!' + channel
+  active_db[user_chan] = active_db.get(user_chan, 0) + reducefn(msg)
   active_db.sync()
   return True # Continue processing this message
 
@@ -20,14 +21,16 @@ def activity(bot, user, channel, args):
   if len(args) == 0:
     args = [user.partition('!')[0]]
   for u in args:
-    v = active_db.get(u.lower(), 0)
+    user_chan = u.lower() + '!' + channel
+    v = active_db.get(user_chan, 0)
     bot.say(channel, "%s has %s %s" % (u, str(v), cmd))
 
 @command('top_'+cmd)
 def top_active(bot, user, channel, args):
   global active_db, cmd
   top = sorted(active_db.items(), key=lambda t: t[1], reverse=True)[0]
-  bot.say(channel, "%s has the most %s with %s" % (top[0], cmd, str(top[1])))
+  user, score = top[0].partition('!')[0], top[1]
+  bot.say(channel, "%s has the most %s with %s" % (user, cmd, str(score)))
 
 def init(bot):
   global active_db, reducefn
