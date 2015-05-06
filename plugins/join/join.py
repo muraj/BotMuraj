bafb0389c7aa76d5f77d9b2f143fdce98c76adc9
muraj <killogge@gmail.com>
2015-05-06 11:55:23 -0700

    Coverting from say to msg

--- plugins/join/join.py
+++ plugins/join/join.py
@@ -6,7 +6,7 @@ def join(bot, user, channel, args):
   """!join <chan1> <chan2> # Tells the bot to join the given channels"""
   nick,_,hostmask = user.partition('!')
   if not bot.isOp(channel, nick):
-    bot.say(channel, 'Op only, sorry')
+    bot.msg(channel, 'Op only, sorry')
     return
 
   for c in args:
@@ -17,7 +17,7 @@ def leave(bot, user, channel, args):
   """!leave <chan1> <chan2> # Tells the bot to leave the given channels"""
   nick,_,hostmask = user.partition('!')
   if not bot.isOp(channel, nick):
-    bot.say(channel, 'Op only, sorry')
+    bot.msg(channel, 'Op only, sorry')
     return
 
   for c in args:
