f9f81b08af619625c83480cfc0211e49ea2cfeb8
muraj <killogge@gmail.com>
2015-05-06 11:56:24 -0700

    Changing soul count to be based on unique messages by utilizing bloom filters
    * Need to expose some config variables for the bloom filter.
    * Need to test bloomfilter implementation

--- plugins/active_mon/active_mon.py
+++ plugins/active_mon/active_mon.py
@@ -1,10 +1,76 @@
 from plugin_lib import command, trigger
 import shelve
 import os.path
+from math import log
+import hashlib
+import mmap
 
 active_db = None
-reducefn = len
+#reducefn = len
 cmd = 'souls'
+bloomFilter = None
+
+def hash_func(s, seed):
+  # TODO: replace with something more efficient
+  return int(hashlib.sha256(s + str(seed)).hexdigest(), base=16)
+
+class BloomFilter(object):
+  def __init__(self, n, p, filename, hash_f=hash_func):
+    self._n, self._p = n, p
+    self._m = int(- self._n * log(self._p) / log(2)**2 ) + 1
+    self._k = int(log(2) * self._m / self._n) + 1
+    self.hash_func = hash_f
+
+    initialize_file = not os.path.exists(filename)
+
+    if not initialize_file:
+      self._file = open(filename, 'r+b')
+    else: # Create the file
+      self._file = open(filename, 'w+b')
+      self._file.truncate((self._m+7) / 8)  # Set the file size
+      self._file.flush()
+
+    self._mmap = mmap.mmap(self._file.fileno(), (self._m+7) / 8, access=mmap.ACCESS_WRITE)
+
+  def close(self):
+    self._mmap.close()
+    self._file.close()
+
+  def setBit(self, i):
+    idx, bit = i >> 3, i & 0x07
+    self._mmap[idx] = chr(ord(self._mmap[idx]) | (1 << bit))
+    #self._mmap.flush(idx,1)  # Sync memory and backing store
+
+  def getBit(self, i):
+    idx, bit = i >> 3, i & 0x07
+    return ((ord(self._mmap[idx]) >> bit) & 1) == 1
+
+  def _hashed_indices(self, s):
+    for seed in range(self._k):
+      h = self.hash_func(s, seed) % self._m
+      yield h
+
+  def add(self, s):
+    for idx in self._hashed_indices(s):
+      self.setBit(idx)
+
+  def maybe_contains(self, s):
+    for idx in self._hashed_indices(s):
+      if not self.getBit(idx):
+        return False
+    return True
+
+  def check_add(self, s):
+    missing = False
+    for idx in self._hashed_indices(s):
+      if not self.getBit(idx):
+        self.setBit(idx)
+        missing = True
+    return not missing
+
+def reducefn(s):
+  global bloomFilter
+  return 0 if bloomFilter.check_add(s) else 1
 
 @trigger('PRIVMSG')
 def activity_track(bot, user, channel, msg):
@@ -36,8 +102,20 @@ def top_active(bot, user, channel, args):
   bot.say(channel, "%s has the most %s with %s" % (user, cmd, str(score)))
 
 def init(bot):
-  global active_db, reducefn
+  global active_db, reducefn, bloomFilter
   ACTIVE_DB_FILE = '~/.config/glitchbot/active.shelve'
+  BLOOMFILTER_MMAP = '~/.config/glitchbot/active.bloom'
   if bot.config.has_option('active_mon', 'db'):
     ACTIVE_DB_FILE = bot.config.get('active_mon', 'db')
+
   active_db = shelve.open(os.path.expanduser(ACTIVE_DB_FILE))
+  bloomFilter = active_db.get('__BLOOMFILTER__', None)
+  if bloomFilter == None:
+    # TODO: move to config file
+    bloomFilter = [5000*365, 0.1, BLOOMFILTER_MMAP]
+
+  active_db['__BLOOMFILTER__'] = bloomFilter
+  active_db.sync()
+
+  bloomFilter[-1] = os.path.expanduser(bloomFilter[-1])
+  bloomFilter = BloomFilter(*bloomFilter)
