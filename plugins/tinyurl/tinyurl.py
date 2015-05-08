d7d683d7ad51a2d4caf46868259e67c8e7be4a8f
muraj <killogge@gmail.com>
2015-05-07 17:32:14 -0700

    Switching to youtube api 3

--- plugins/tinyurl/tinyurl.py
+++ plugins/tinyurl/tinyurl.py
@@ -1,6 +1,5 @@
 from twisted.web.client import getPage
 from plugin_lib import trigger
-import xml.etree.ElementTree
 import json
 import re
 
@@ -13,10 +12,15 @@ def parse_tinyurl(body, bot, channel):
   bot.say(channel, 'Tiny: ' + tiny.encode('utf8'))
 
 def parse_youtube(body, bot, channel, vid):
-  e = xml.etree.ElementTree.fromstring(body)
-  title = e.findtext('{http://www.w3.org/2005/Atom}title').encode('utf8')
+  print 'parsing youtube body', body
+  body = json.loads(body)
+  video_list = body.get(u'items',[])
+  title = ''
+  if len(video_list) > 0:
+    title = video_list[0].get(u'snippet', {}).get(u'title', '').encode('utf8')
+    if len(title) > 0: title += ' - '
   url = 'http://youtu.be/' + vid.encode('utf8')
-  bot.say(channel, "Watch! (or don't, it probably sucks): %s - %s" % (title, url))
+  bot.say(channel, "Watch! (or don't, it probably sucks): %s%s" % (title, url))
 
 @trigger('PRIVMSG', match=r'.*https?:\/\/\S+.*')
 def tinyurl(bot, user, channel, msg):
@@ -35,7 +39,7 @@ def tinyurl(bot, user, channel, msg):
     youtube_groups = re.search(r'be\/([^?]+)', url)
   if youtube_groups:
     vid = youtube_groups.group(1)
-    d = getPage("https://gdata.youtube.com/feeds/api/videos/%s?v=2" % vid)
+    d = getPage("https://www.googleapis.com/youtube/v3/videos?key=%s&id=%s&part=snippet" % (APIKEY, vid))
     d.addCallback(parse_youtube, bot, channel, vid)
   elif len(url) < MINLEN:
     return
